5b2ae3eebf5266b7f3cd59cac3a6ebbee30f7a8f
Haroon <81903831+Dawood-Siddique@users.noreply.github.com>
2025-09-04 02:05:09 +0500

    Update settings.py

--- insurance-be/settings.py
+++ insurance-be/settings.py
@@ -145,8 +145,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 
